8ce4b64c8811a2f2f4143cf5cd7d62f1ddb7c0de
Коннов Игорь <ikonnoff@mail.ru>
2022-11-08 04:37:02 +0300

    Переопределение метода перевода со счёта на счёт

--- src/ru/geekstar/Account/SberPayCardAccount.java
+++ src/ru/geekstar/Account/SberPayCardAccount.java
@@ -13,4 +13,14 @@ public class SberPayCardAccount extends PayCardAccount {
         // прибавим сумму перевода к общей сумме всех переводов клиентам Сбера без комиссии за месяц для контроля лимита
         getAccountHolder().updateTotalTransfersToClientSberWithoutCommissionMonthInRUB(toCard, sumTransfer);
     }
+
+    @Override
+    public void transferAccount2Account(SberSavingsAccount toAccount, float sumTransfer) {
+        // вызываем родительскую версию метода
+        super.transferAccount2Account(toAccount, sumTransfer);
+        // и дополняем метод уникальным поведением
+        // прибавим сумму перевода к общей сумме всех переводов клиентам Сбера без комиссии за месяц для контроля лимита
+        getAccountHolder().updateTotalTransfersToClientSberWithoutCommissionMonthInRUB(toAccount, sumTransfer);
+    }
+    
 }
--- src/ru/geekstar/Account/SberSavingsAccount.java
+++ src/ru/geekstar/Account/SberSavingsAccount.java
@@ -14,4 +14,13 @@ public class SberSavingsAccount extends SavingsAccount {
         getAccountHolder().updateTotalTransfersToClientSberWithoutCommissionMonthInRUB(toCard, sumTransfer);
     }
 
+    @Override
+    public void transferAccount2Account(SberSavingsAccount toAccount, float sumTransfer) {
+        // вызываем родительскую версию метода
+        super.transferAccount2Account(toAccount, sumTransfer);
+        // и дополняем метод уникальным поведением
+        // прибавим сумму перевода к общей сумме всех переводов клиентам Сбера без комиссии за месяц для контроля лимита
+        getAccountHolder().updateTotalTransfersToClientSberWithoutCommissionMonthInRUB(toAccount, sumTransfer);
+    }
+
 }
