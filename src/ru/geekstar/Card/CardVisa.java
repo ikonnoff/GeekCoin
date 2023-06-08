c4f8e4f35be9819dc5f0f3b4c480f44ee272b33d
Коннов Игорь <ikonnoff@mail.ru>
2023-06-08 19:55:50 +0400

    Добавил курсы валют платёжных систем

--- src/ru/geekstar/Card/CardVisa.java
+++ src/ru/geekstar/Card/CardVisa.java
@@ -18,7 +18,8 @@ public abstract class CardVisa extends Card implements IVisa {
         // по умолчанию null, потому что не во всех странах может использоваться данная платёжная система
         String billingCurrencyCode = null;
         // если покупка в Турции, то валюта биллинга в $
-        if (country.equalsIgnoreCase("Турция")) billingCurrencyCode = CURRENCY_CODE_PAY_SYSTEM_USD;
+        if (country.equalsIgnoreCase("Турция") || country.equalsIgnoreCase("Казахстан")
+                || country.equalsIgnoreCase("Франция")) billingCurrencyCode = CURRENCY_CODE_PAY_SYSTEM_USD;
 
         return billingCurrencyCode;
     }
@@ -27,11 +28,25 @@ public abstract class CardVisa extends Card implements IVisa {
     public ArrayList<Float> getExchangeRatePaySystem(String currency, String currencyExchangeRate) {
         // TODO: Запрос к API Visa
         ArrayList<Float> exchangeRatePaySystem = new ArrayList<>();
-        // курс лиры к доллару
+
+        // курс лиры в долларах
         if (currency.equals("TRY") && currencyExchangeRate.equals("USD")) {
-            exchangeRatePaySystem.add(0.056f); // курс покупки
-            exchangeRatePaySystem.add(0.046f); // курс продажи
+            exchangeRatePaySystem.add(0.0448f); // курс покупки
+            exchangeRatePaySystem.add(0.0496f); // курс продажи
         }
+
+        // курс тенге в долларах
+        if (currency.equals("KZT") && currencyExchangeRate.equals("USD")) {
+            exchangeRatePaySystem.add(0.0021f);
+            exchangeRatePaySystem.add(0.0023f);
+        }
+
+        // курс евро в долларах
+        if (currency.equals("EUR") && currencyExchangeRate.equals("USD")) {
+            exchangeRatePaySystem.add(1.024f);
+            exchangeRatePaySystem.add(1.132f);
+        }
+
         return exchangeRatePaySystem;
     }
 
