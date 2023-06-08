ee66f78ee3cf9724d377346c37536680474f416f
Коннов Игорь <ikonnoff@mail.ru>
2023-06-08 17:16:05 +0400

    Пофиксил баг из-за которого не отображалось с какого счёта или карты было пополнение, если пополнение между картами одного банка

--- src/ru/geekstar/Transaction/DepositingTransaction.java
+++ src/ru/geekstar/Transaction/DepositingTransaction.java
@@ -28,28 +28,32 @@ public class DepositingTransaction extends Transaction {
 
     @Override
     public String getSender() {
-        String sender = "";
-
-        // если списание происходит с карты
-        if (getFromCard() != null) {
-            // и если мы пополняем чужую карту или счёт (или свою карту или счёт, но в другом банке), то добавляем в транзакцию имя отправителя перевода
-            if ((getToCard() != null && !getFromCard().getCardHolder().isClientCard(getToCard())) || (getToAccount() != null && !getFromCard().getCardHolder().isClientAccount(getToAccount()))) {
-                PhysicalPerson cardHolder = getFromCard().getCardHolder().getPhysicalPerson();
-                sender = cardHolder.getFirstName() + " " + cardHolder.getLastName().substring(0, 1);
-            }
-        }
-
-        // если списание происходит со счёта
-        if (getFromAccount() != null) {
-            // и если мы пополняем чужую карту или счёт (или свою карту или счёт, но в другом банке), то добавляем в транзакцию имя отправителя перевода
-            if ((getToCard() != null && !getFromAccount().getAccountHolder().isClientCard(getToCard())) || (getToAccount() != null && !getFromAccount().getAccountHolder().isClientAccount(getToAccount()))) {
-                PhysicalPerson accountHolder = getFromAccount().getAccountHolder().getPhysicalPerson();
-                sender = accountHolder.getFirstName() + " " + accountHolder.getLastName().substring(0, 1);
-            }
-        }
+        String sender = super.getSender();
 
         if (!sender.isEmpty()) {
-            sender = super.getSender() + " от " + sender + ".";
+            String holder = "";
+
+            // если списание происходит с карты
+            if (getFromCard() != null) {
+                // и если мы пополняем чужую карту или счёт (или свою карту или счёт, но в другом банке), то добавляем в транзакцию имя отправителя перевода
+                if ((getToCard() != null && !getFromCard().getCardHolder().isClientCard(getToCard())) || (getToAccount() != null && !getFromCard().getCardHolder().isClientAccount(getToAccount()))) {
+                    PhysicalPerson cardHolder = getFromCard().getCardHolder().getPhysicalPerson();
+                    holder = cardHolder.getFirstName() + " " + cardHolder.getLastName().substring(0, 1);
+                }
+            }
+
+            // если списание происходит со счёта
+            if (getFromAccount() != null) {
+                // и если мы пополняем чужую карту или счёт (или свою карту или счёт, но в другом банке), то добавляем в транзакцию имя отправителя перевода
+                if ((getToCard() != null && !getFromAccount().getAccountHolder().isClientCard(getToCard())) || (getToAccount() != null && !getFromAccount().getAccountHolder().isClientAccount(getToAccount()))) {
+                    PhysicalPerson accountHolder = getFromAccount().getAccountHolder().getPhysicalPerson();
+                    holder = accountHolder.getFirstName() + " " + accountHolder.getLastName().substring(0, 1);
+                }
+            }
+
+            if (!holder.isEmpty()) {
+                sender += " от " + holder + ".";
+            }
         }
 
         return sender;
